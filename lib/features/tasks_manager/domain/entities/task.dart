a9f372305b5a80f2289dde1f25ed384c09b1ee6f
Denis Stahovsky <denis.stahovsky@vantino.ch>
2020-02-26 06:56:50 +0200

    added entities for User and Task

---
+++ lib/features/tasks_manager/domain/entities/task.dart
@@ -0,0 +1,26 @@
+import 'package:meta/meta.dart';
+
+///
+/// Task entity
+///
+class Task {
+
+  final int id;
+  String title;
+  int dueBy;
+  Priority priority;
+  String description;
+
+  Task({
+    @required this.id,
+    @required this.title,
+    this.dueBy,
+    this.priority,
+    this.description
+  });
+
+}
+
+enum Priority {
+  High, Medium, Low
+}
---
+++ lib/features/tasks_manager/domain/entities/user.dart
@@ -0,0 +1,12 @@
+///
+/// User entity, just to keep auth token
+///
+class User {
+
+  // auth token
+  // The token will expire in 24 hours
+  final String token;
+
+  User({this.token});
+
+}
