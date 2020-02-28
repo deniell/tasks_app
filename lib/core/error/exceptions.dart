a651b30fbe6add1253f00cd30a11c157cb1b2a77
Denis Stahovsky <denis.stahovsky@vantino.ch>
2020-02-28 07:36:34 +0200

    added exceptions entities

---
+++ lib/core/error/exceptions.dart
@@ -0,0 +1,19 @@
+// throw for 500
+class ServerException implements Exception {
+  final String message = "Server Failure.";
+}
+
+// throw for 422
+class ValidationException implements Exception {
+  final String message = "Validation failed.";
+}
+
+// throw for 403
+class UnauthorizedException implements Exception {
+  final String message = "This action is unauthorized.";
+}
+
+// throw for default
+class UnexpectedException implements Exception {
+  final String message = "Something went wrong.";
+}
